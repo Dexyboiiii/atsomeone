d9b9d9d52678fd828bf7427ad37c78d3f851b12b
Dexyboiiii <50030862+Dexyboiiii@users.noreply.github.com>
2020-08-26 22:25:08 +0100

    Added what will become an opt-in for atsomeone It doesn't work yet, and modifying to files needs to be implemented Right now it just changes an array

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Create an auth.json file in the root, with
 inside of it.
 
 By default, bot.js has the bit that actually mentions people commented out because it's goddamn annoying - switch the commented line to the second one and it will mention people
+
+# Things I'd like to do:
+OKAY SO
+- List of people who've specifically opted into being atsomeone'd
+    - List of people who've opted out?
+    - Could have a switch between mentioning those who've either been opted in or not opted out?
+- Replicating the message the original author sent with the person atsomeone chose to mention included in it
+- Would like to be able to mention one person from a specific role without it mentioning the whole role?
+- Adding a help command
+    - And a status of how to call it?
+- 
--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+var optedIn = [];
 
 async function getRandomIDFromMessage(message) {
     try {
         var members = await message.guild.members.fetch();
         let userIDs = members.keyArray();
-        var randomID = userIDs[Math.floor(Math.random()*userIDs.length)];
+        var randomID = userIDs[Math.floor(Math.random() * userIDs.length)];
         return randomID;
     } catch (e) {
         console.log("oof" + e);
@@ -30,19 +31,49 @@ function atSomeoneMessage(message, actuallyMention) {
     }
 }
 
+// This is a rough approximation of what the function *should* do
+// One day it'll open a file, make a few changes to it, and close it again so it at least survives the bot being shut down
+function optSomeoneIn(guildID, authorID) {
+    for (i = 0; i < optedIn.length; i++) {
+        if (optedIn[i][0] == guildID) {
+            // Find the authorID in the array. If it's not there, put it in. If it is, remove it.
+            let authorIDIndex = optedIn[i][1].indexOf(authorID);
+            if (authorIDIndex != -1) {
+                optedIn[i][1].push(authorID);
+                return;
+            } else {
+                delete optedIn[i][1][authorIDIndex];
+            }
+            
+        }
+    }
+    optedIn.push([guildID, [authorID]]);
+    return;
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+    // todo: load the opt-in json and keep it as an array
 });
 
 client.login(auth.token);
 
 //TODO: make it run when mentioned, not when a message contains @someone
 client.on("message", message => {
-    if (message.content.includes("@someone")) {
+    if (message.content.includes("@someone-everyone")) {
         try {
             atSomeoneMessage(message, false);
         } catch (error) {
             console.log(error);
         }
+    } else if (message.content.includes("@someone-optin")) {
+        try {
+            optSomeoneIn(message.guild.id, message.author.id);
+            console.log(optedIn);
+        } catch (error) {
+            console.log(error);
+        }
+    } else if (message.content.includes("@someone")) {
+        // Look up the list of those who've either opted in or not opted out and mention one at random
     }
 });
